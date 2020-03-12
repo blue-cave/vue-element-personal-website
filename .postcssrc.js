eaea3e567c1ae28d1f85e0c66d60afc9f853f513
@xusy <spookz@qq.com>
2020-03-12 11:04:35 +0800

    2020-3-12,页面底部添加标识

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,26 +6,26 @@ module.exports = {
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
     // 由于cssnext也具有autoprefixer,只需要一个就够了，所以把默认的autoprefixer删除掉
-    // "autoprefixer": {},
-    // 下面是自适应
-    "postcss-aspect-ratio-mini": {},
-    "postcss-write-svg": {
-      utf8: false
-    },
-    "postcss-cssnext": {},
-    "postcss-px-to-viewport": {
-      viewportWidth: 1920,     //  视窗的宽度，对应的是我们设计稿的宽度，移动端一般是750，如果是pc端那就是类似1920这样的尺寸
-      viewportHeight: 1080,    // 视窗的高度，移动端一般指定1334，也可以不配置，如果是pc端那就是类似1080这样的尺寸
-      unitPrecision: 3,       // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
-      viewportUnit: 'vw',    // 指定需要转换成的视窗单位，建议使用vw
-      selectorBlackList: ['.ignore', '.hairlines'],  // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-      minPixelValue: 1,      // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-      mediaQuery: false      // 允许在媒体查询中转换`px`
-    },
-    "postcss-viewport-units":{},
-    "cssnano": {
-      preset: "default", // 设置成default将不会启用autoprefixer
-      "postcss-zindex": false // postcss-zindex设置成false，如果不这样做，z-index的值就会重置为1，所以千万记得将postcss-zindex设置为false。
-    }
+    "autoprefixer": {}
+    // TODO 下面是自适应,目前先不用后续再说，因为这里有点冲突
+    // "postcss-aspect-ratio-mini": {},
+    // "postcss-write-svg": {
+    //   utf8: false
+    // },
+    // "postcss-cssnext": {},
+    // "postcss-px-to-viewport": {
+    //   viewportWidth: 1920,     //  视窗的宽度，对应的是我们设计稿的宽度，移动端一般是750，如果是pc端那就是类似1920这样的尺寸
+    //   viewportHeight: 1080,    // 视窗的高度，移动端一般指定1334，也可以不配置，如果是pc端那就是类似1080这样的尺寸
+    //   unitPrecision: 3,       // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+    //   viewportUnit: 'vw',    // 指定需要转换成的视窗单位，建议使用vw
+    //   selectorBlackList: ['.ignore', '.hairlines'],  // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+    //   minPixelValue: 1,      // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+    //   mediaQuery: false      // 允许在媒体查询中转换`px`
+    // },
+    // "postcss-viewport-units":{},
+    // "cssnano": {
+    //   preset: "default", // 设置成default将不会启用autoprefixer
+    //   "postcss-zindex": false // postcss-zindex设置成false，如果不这样做，z-index的值就会重置为1，所以千万记得将postcss-zindex设置为false。
+    // }
   }
 }
--- src/pages/home-pages/home-page.vue
+++ src/pages/home-pages/home-page.vue
@@ -23,19 +23,56 @@
         </el-col>
         <el-col :span="4"></el-col>
       </el-row>
-<!--      第二部分-->
-      <el-row>
-        <img style="height: 200px; width: 200px; border-radius: 50%;" src="../../assets/images/head_portrait.png">
+      <!-- 第二部分 绝对定位于窗口-->
+      <el-row type="flex" class="myself-absolute-location">
+        <img style="height: 62px; width: 62px; border-radius: 50%; margin: 0px 10px;" src="../../assets/images/head_portrait.png">
+        <div> hello </div>
       </el-row>
-<!--      最后一部分-->
-      <el-row>
+      <!-- 显示弹出的一些个人信息 -->
+      <el-row type="flex" justify="center" class="el-avatar-background">
         <div>
-          <a href="https://github.com/blue-cave" target="_blank">
-            <el-tooltip effect="dark" content="Git Hub" placement="top">
-              <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+          <el-link :underline="false">
+            <el-tooltip effect="light" placement="top">
+              <div slot="content"><img class="pop-up-show-img-width" src="../../assets/images/qq.jpg"></div>
+              <el-avatar> QQ </el-avatar>
             </el-tooltip>
-          </a>
+          </el-link>
         </div>
+        <div>
+          <el-link :underline="false">
+            <el-tooltip effect="light" placement="top">
+              <div slot="content"><img class="pop-up-show-img-width" src="../../assets/images/wechat.jpg"></div>
+              <el-avatar> 微信 </el-avatar>
+            </el-tooltip>
+          </el-link>
+        </div>
+        <div>
+          <el-link :underline="false" href="https://github.com/blue-cave" target="_blank">
+            <!-- 通过设置effect属性来改变el-tooltip的主题，默认为dark -->
+            <el-tooltip effect="light" content="Git Hub" placement="top">
+              <el-avatar> Git </el-avatar>
+            </el-tooltip>
+          </el-link>
+        </div>
+        <div>
+          <el-link :underline="false" href="https://www.jianshu.com/u/398d3f4b09a2" target="_blank">
+            <el-tooltip effect="light" content="简书" placement="top">
+              <el-avatar> 简 </el-avatar>
+            </el-tooltip>
+          </el-link>
+        </div>
+        <div>
+          <el-link :underline="false" href="https://blog.csdn.net/qq_45376958" target="_blank">
+            <el-tooltip effect="light" content="CSDN" placement="top">
+              <el-avatar> CSDN </el-avatar>
+            </el-tooltip>
+          </el-link>
+        </div>
+      </el-row>
+      <!-- 最后一行备注 -->
+      <el-row class="statement-bottom-site">
+        本网站有部分内容来自互联网，如无意中侵犯了哪个媒体、公司、企业或个人等的知识产权，请邮箱联系：
+        <el-link style="cursor: pointer;" type="primary" href="mailto: spookz@qq.com" :underline="false">spookz@qq.com</el-link>
       </el-row>
     </div>
 </template>
@@ -65,6 +102,12 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    // 图片加载失败
+    errorHandler () {
+      return true
+    }
   }
 }
 </script>
@@ -76,4 +119,56 @@ export default {
     height: 400px;
     width: 1200px;
   }
+</style>
+
+<style>
+.myself-absolute-location {
+  cursor: pointer;
+  width: 250px;
+  height: 64px;
+  position: fixed;
+  right: 20px;
+  top:30px;
+  background:rgba(255,255,255,0.5);
+  border-radius: 5px;
+  border: 1px #ccc solid;
+}
+/* 这是鼠标移入的弹出qq，微信的图片的大小 */
+.pop-up-show-img-width {
+  width: 130px;
+  height: 150px;
+}
+.pop-up-div-img-width{
+  height: 38px;
+  width: 38px;
+  background-color: #FFFFFF;
+  border-radius: 50%;
+  position: relative;
+}
+/* 在鼠标移入之前页面显示的图片 */
+.pop-up-img-width {
+  height: 28px;
+  width: 28px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+}
+/* 个人网站的底部声明 */
+.statement-bottom-site {
+  padding: 15px 0px;
+  background-color: #2c3e50;
+  color: #FFFFFF;
+  font-size: 14px;
+}
+/* 上层div的边距 10px */
+.el-avatar-background div {
+  padding: 10px;
+}
+/* el-avatar 的背景色*/
+.el-avatar-background .el-avatar {
+  background: #404040;
+}
 </style>
